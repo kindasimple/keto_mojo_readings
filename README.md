fd1caa4d586178a1c2a04c19c8e96b06220eeefe
Evan White <kindasimple@gmail.com>
2019-04-29 00:42:48 -0700

    initial import

---
+++ README.md
@@ -0,0 +1,6 @@
+keto_mojo_readings
+=========
+
+Convert a Keto Mojo glucose/ketone/hematocrit/hemoglobin export to a series of readings based on temporal proximity
+
+The resulting CSV file is the input for a google sheets document: https://docs.google.com/spreadsheets/d/1bKP0rKxLVvfmrYzn4RfKXkJsmHoRDLVjqHeCii7iQGs/edit?usp=sharing 
---
+++ process_ketomojo.py
@@ -0,0 +1,84 @@
+#!/usr/bin
+
+import csv
+from datetime import datetime
+from collections import defaultdict
+import pytz
+
+
+LOCAL_TIMEZONE = 'America/Los_Angeles'
+MAX_READING_TIME_DIFF = 60 * 10  # 10 minutes
+
+FIELDNAMES = ['type', 'value', 'unit', 'date', 'time']
+VALUE_TYPES = ['Glucose', 'Ketone', 'Hematocrit', 'Hemoglobin', 'Create Date', 'Update Date']
+
+
+def reading_dict_to_date(reading):
+    naive_datetime = datetime.strptime(
+        '{}{}'.format(reading['date'], reading['time']).strip()
+        , '%m/%d/%y %I:%M %p'
+    )
+    pst = pytz.timezone(LOCAL_TIMEZONE)
+    return pst.localize(naive_datetime)
+ 
+
+def gather_input(input_file_path):
+    """
+    iterate over readings in the input file and aggregate data points
+    captured within a 15 minute window, averaging readings of the same 
+    type.
+    """
+    readings = []
+    last_reading_date = None
+    with open(input_file_path) as infile:
+        infile.next()
+        data = csv.DictReader(infile, fieldnames=FIELDNAMES)
+        reading_data = defaultdict(list)
+        reading_update_date = None
+        for datum in data:
+            # get the date from the reading dictionary
+            datum_date = reading_dict_to_date(datum)
+            if not reading_update_date:
+                reading_create_date = reading_update_date = datum_date
+            readings_diff_seconds= (datum_date - reading_update_date).total_seconds()
+            if abs(readings_diff_seconds) > MAX_READING_TIME_DIFF:
+                # if the readings are far apart add the reading
+                reading = calculate_reading(reading_data, reading_create_date, reading_update_date)
+                readings.append(reading)
+                # reset the reading create a new reading
+                reading_data = defaultdict(list)
+                reading_create_date = datum_date
+            reading_data[datum['type']].append(datum['value'])
+            reading_update_date = datum_date
+        reading = calculate_reading(reading_data, reading_create_date, reading_update_date)
+        readings.append(reading)
+    return readings
+
+
+def write_output(output_file_path, readings):
+    """Write readings to an output csv file"""
+    with open(output_file_path, 'w') as outfile:
+        writer = csv.DictWriter(outfile, VALUE_TYPES)
+        writer.writeheader()
+        writer.writerows(readings)
+
+
+def calculate_reading(data_type_to_values, create_date, update_date):
+    """
+    Average readings for each type
+    """
+    data = {
+        reading_type: sum([float(v) for v in values])/len(values) or None
+        for reading_type, values in data_type_to_values.items()
+    }
+    data['Create Date'] = create_date.strftime('%m-%d-%Y %H:%m')
+    data['Update Date'] = update_date.strftime('%m-%d-%Y %H:%m')
+    return data
+
+
+if __name__ == '__main__':
+    input_file = 'KetoMojo.csv'
+    output_file = 'keto_mojo_readings.csv'
+    readings = gather_input(input_file)
+    write_output(output_file, readings)
+
